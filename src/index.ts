b5bce205aa1429d8f869b73573a68c6081d8c2e8
Eliot Partridge <shortcircuit369@gmail.com>
2019-03-02 21:57:08 -0600

    More project setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import * as Discord from 'discord.js';
 import { Fetch } from 'node-derpi';
 
-const config = require('../config.json');
+interface BotConfig {
+	discord: {
+		token: string;
+	};
+	derpi: {
+		apiKey: string;
+		bannedTags: {
+			nsfw: string[],
+			sfw: string[],
+			both: string[]
+		}
+	};
+}
+
+const config = require('../config.json') as BotConfig;
+
+const client = new Discord.Client();
+
+client.login(config.discord.token);
