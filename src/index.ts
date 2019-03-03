e0eac78db0768f9a5812a6fe8b1762da0dedb28c
Eliot Partridge <shortcircuit369@gmail.com>
2019-03-02 23:33:32 -0600

    IT LIVES!

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,51 @@ interface BotConfig {
 	};
 }
 
+function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const config = require('../config.json') as BotConfig;
 
 const client = new Discord.Client();
+
+client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', msg => {
+		if (process.env.NODE_ENV !== 'production') {
+			console.log(msg);
+		}
+	});
+
+client.on('message', async msg => {
+	if (msg.channel.type !== 'text') return;
+	if (msg.attachments.size < 1) return;
+
+	await sleep(200);
+
+	const nsfwChannel = (msg.channel as Discord.TextChannel).nsfw;
+	const appropriateTagSet = nsfwChannel ? config.derpi.bannedTags.nsfw : config.derpi.bannedTags.sfw;
+	const bannedTags = config.derpi.bannedTags.both.concat(appropriateTagSet);
+
+	for (const attachment of msg.attachments.values()) {
+		const url = attachment.url;
+
+		console.log(`${msg.id} - Processing '${url}'`);
+
+		const reverseImageResults = await Fetch.reverseImageSearch({
+			key: config.derpi.apiKey,
+			url: url,
+			fuzziness: 0.2
+		});
+
+		for (const result of reverseImageResults.images) {
+			if (result.tagString.split(', ').some(tag => bannedTags.includes(tag)) && msg.deletable) {
+				await msg.delete();
+				return msg.reply(`your message was removed for containing images with one of the following tags: ${bannedTags.join(', ')}`);
+			}
+		}
+	}
+});
 
 client.login(config.discord.token);
