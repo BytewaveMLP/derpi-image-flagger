5fb2f13829764014483a5966e677173b6dae637a
Eliot Partridge <shortcircuit369@gmail.com>
2019-03-04 22:15:10 -0600

    Process message links

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as Discord from 'discord.js';
 import { Fetch } from 'node-derpi';
+import getUrls = require('get-urls');
+import * as URL from 'url';
+import * as path from 'path';
 
 interface BotConfig {
 	discord: {
@@ -15,15 +18,26 @@ interface BotConfig {
 	};
 }
 
+const IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif'].map(ext => `.${ext}`);
+
 function sleep(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const config = require('../config.json') as BotConfig;
 
-const client = new Discord.Client();
+const client = new Discord.Client({
+	disabledEvents: [
+		'TYPING_START'
+	],
+	disableEveryone: true
+});
 
 client
+	.on('ready', () => {
+		console.log(`Ready at ${(new Date()).toISOString()}`);
+		console.log(`Logged in as: @${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	})
 	.on('error', console.error)
 	.on('warn', console.warn)
 	.on('debug', msg => {
@@ -32,34 +46,63 @@ client
 		}
 	});
 
-client.on('message', async msg => {
-	if (msg.channel.type !== 'text') return;
-	if (msg.attachments.size < 1) return;
+async function isImageSafe(url: string, bannedTags: string[]): Promise<boolean> {
+	const urlPath = URL.parse(url).pathname;
+	if (!urlPath) return true; // we can't tell
 
-	await sleep(200);
+	const ext = path.extname(urlPath);
+	if (!ext || !IMAGE_EXTENSIONS.includes(ext)) return true; // probably not an image
+
+	const reverseImageResults = await Fetch.reverseImageSearch({
+		key: config.derpi.apiKey,
+		url: url,
+		fuzziness: 0.2
+	});
+
+	for (const result of reverseImageResults.images) {
+		if (result.tagString.split(', ').some(tag => bannedTags.includes(tag))) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+client.on('message', async msg => {
+	if (msg.author.bot) return;
+	if (msg.channel.type !== 'text') return;
+	if (msg.attachments.size >= 1) await sleep(200); // allow attachments some time to propagate to CDN
 
 	const nsfwChannel = (msg.channel as Discord.TextChannel).nsfw;
 	const appropriateTagSet = nsfwChannel ? config.derpi.bannedTags.nsfw : config.derpi.bannedTags.sfw;
 	const bannedTags = config.derpi.bannedTags.both.concat(appropriateTagSet);
 
+	const messageLinks = getUrls(msg.content);
+
+	for (const url of messageLinks) {
+		console.log(`${msg.id} - [text] Processing '${url}'`);
+
+		if (!await isImageSafe(url, bannedTags) && msg.deletable) {
+			await msg.delete();
+			console.log(`${msg.id} - Message is NOT clean. Removing...`);
+			return msg.reply(`your message was removed for containing images with one of the following tags: \`${bannedTags.join(', ')}\``);
+		}
+	}
+
 	for (const attachment of msg.attachments.values()) {
 		const url = attachment.url;
 
-		console.log(`${msg.id} - Processing '${url}'`);
+		console.log(`${msg.id} - [attach] Processing '${url}'`);
 
-		const reverseImageResults = await Fetch.reverseImageSearch({
-			key: config.derpi.apiKey,
-			url: url,
-			fuzziness: 0.2
-		});
-
-		for (const result of reverseImageResults.images) {
-			if (result.tagString.split(', ').some(tag => bannedTags.includes(tag)) && msg.deletable) {
-				await msg.delete();
-				return msg.reply(`your message was removed for containing images with one of the following tags: ${bannedTags.join(', ')}`);
-			}
+		if (!await isImageSafe(url, bannedTags) && msg.deletable) {
+			await msg.delete();
+			console.log(`${msg.id} - Message is NOT clean. Removing...`);
+			return msg.reply(`your message was removed for containing images with one of the following tags: \`${bannedTags.join(', ')}\``);
 		}
 	}
+
+	console.log(`${msg.id} - Message is probably clean.`);
 });
 
-client.login(config.discord.token);
+client.login(config.discord.token)
+	.catch(console.error);
